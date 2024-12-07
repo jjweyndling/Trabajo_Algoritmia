d60ffb1a142396a641737567eff404465fd7a284
larrayoz146585 <148328956+larrayoz146585@users.noreply.github.com>
2024-12-07 09:48:28 +0100

    Update clasificar.h

--- src/clasificar.h
+++ src/clasificar.h
@@ -3,24 +3,24 @@
 
 #include "maxMonticulo.h"
 
-#define PESO_APERTURA 2
-#define PESO_VALORMAX 2
-#define PESO_VALORMIN 2
-#define PESO_CIERRE 3
+#define PESO_APERTURA 1
+#define PESO_VALORMAX 1
+#define PESO_VALORMIN 1
+#define PESO_CIERRE 1
 #define PESO_VOLUMEN 1
-#define PESO_RSI7 0.5
-#define PESO_RSI14 0.5
-#define PESO_CCI7 0.5
-#define PESO_CCI14 0.5
+#define PESO_RSI7 1
+#define PESO_RSI14 1
+#define PESO_CCI7 1
+#define PESO_CCI14 1
 #define PESO_SMA50 1
 #define PESO_EMA50 1
 #define PESO_SMA100 1
 #define PESO_EMA100 1
-#define PESO_MACD 0.8
-#define PESO_BOLLINGER 0.7
-#define PESO_TR 0.6
-#define PESO_ATR7 0.8
-#define PESO_ATR14 0.8
+#define PESO_MACD 1
+#define PESO_BOLLINGER 1
+#define PESO_TR 1
+#define PESO_ATR7 1
+#define PESO_ATR14 1
 
 
 void seleccionarKNN(listaStock, tipoMaxMonticulo *, t_stock);
