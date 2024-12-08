c0dfb7485af8a9917ae9fc5b47b6a4da4cd6d38f
larrayoz146585 <148328956+larrayoz146585@users.noreply.github.com>
2024-12-08 20:06:34 +0100

    graficosbien

--- data/graficos.py
+++
@@ -1,9 +0,0 @@
-#quiero hacer un grafico con los datos del archivo data.csv
-import matplotlib.pyplot as plt
-import pandas as pd
-
-data = pd.read_csv('data/data.csv')
-plt.plot(data['x'], data['y'])
-plt.show()
-
-
